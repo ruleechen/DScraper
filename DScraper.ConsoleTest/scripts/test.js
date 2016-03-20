e287832b8b6059cb0cf33d55b86faf1e91b6c08e
Rulee <rulee@live.cn>
2016-03-20 10:45:55 +0800

    update

--- DScraper.ConsoleTest/scripts/test.js
+++ DScraper.ConsoleTest/scripts/test.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 
 var casper = require('casper').create({
     clientScripts: ['includes/jquery.js'],
@@ -21,6 +22,8 @@ casper.start('http://www.cnblogs.com/');
 casper.then(function (response) {
 
     debugger;
+
+    fs.write('page.html', this.getPageContent(), 'w');
 
     //this.echo('test:' + args.test)
 
