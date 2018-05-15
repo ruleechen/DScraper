c106b2b540a8a82bdacc45c06fbf7f7c5796f4f6
Rulee Chen <rulee@live.cn>
2018-05-15 15:21:08 +0800

    run start.cmd silently

--- start.cmd
+++ start.cmd
@@ -1,3 +1,11 @@
+@echo off
+
+if "%1"=="h" goto begin
+
+mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&&exit
+
+:begin
+
 cd /D %~dp0
 
 cnpm install & npm run start
